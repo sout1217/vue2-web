b7895ea3a3f157c516004b296dc7cc2144bd74e9
DESKTOP-TRJT0MI\Admin <sout1217@gmail.com>
2020-09-13 16:21:45 +0900

    using vuex with axios

--- src/components/SlideBanner.vue
+++ src/components/SlideBanner.vue
@@ -11,15 +11,11 @@
 
             <!-- The slideshow -->
             <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img :src="banners[0].download_url" alt="Los Angeles">
-                </div>
-                <div class="carousel-item">
-                    <img :src="banners[1].download_url" alt="Chicago">
-                </div>
-                <div class="carousel-item">
-                    <img :src="banners[2].download_url" alt="New York">
-                </div>
+                <template v-for="(banner, index) in banners" >
+                    <div :class="['carousel-item', index===0?'active':'']" v-bind:key="banner.id">
+                        <img :src="banner.download_url" alt="Los Angeles">
+                    </div>
+                </template>
             </div>
 
             <!-- Left and right controls -->
@@ -35,23 +31,34 @@
 </template>
 
 <script>
-    import banners from "../api/banners";
+    import {mapState} from "vuex";
 
     export default {
         name: "SlideBanner",
         data() {
-            return {
-                banners: []
-            }
+            return {}
+        },
+        computed: {
+            /* namespaced: true 이용한 방법 */
+            ...mapState('bannersModule', {
+                banners(state) {
+                    return state.mainBanners.splice(0,3)
+                }
+            })
+
+            /* namespaced: false 이용한 방법 */
+            // ...mapState({
+            //     banners(state) {
+            //         return state.bannersModule.mainBanners
+            //     }
+            // })
         },
         created() {
-            banners.getMainSlideBanners()
-                .then(res => {
-                    this.banners = [].concat(res.data) // 깊은 복사 (deep copy)
-                    // this.banners = res.data 는 얕은 복사 (shallow copy) 이다
-                    // 때문에, res.data.push('add new data') 이와 같이 추가 하는 경우 this.banners 까지 영향을 미친다
-                    // 반면 깊은 복사인 [].concat(res.data) 는 push 를 하여도 영향을 미치지 않는다
-                })
+            console.log(this.$store)
+            /* namespaced: true 이용한 방법 */
+            this.$store.dispatch("bannersModule/setMainBanners")
+            /* namespaced: false 이용한 방법 */
+            // this.$store.dispatch("setMainBanners")
         }
     }
 </script>
